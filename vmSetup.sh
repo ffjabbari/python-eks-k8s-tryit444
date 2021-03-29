bd4f448cf174afc29f22fff5ab9b1dbda843f04a
mkornyev <mkornyev@gmail.com>
2021-03-29 10:49:15 -0400

    Added test script (ran it on vagrant startup)

--- vmSetup.sh
+++ vmSetup.sh
@@ -9,11 +9,15 @@ sudo apt-get install npm
 
 echo "Copying Over Assets"
 
-cp /vagrant/bookApp/{index.js,db-connector.js,package.json,Dockerfile} .
+cp /vagrant/bookApp/{index.js,db-connector.js,package.json,Dockerfile,test_endpoints.json} .
 
 echo "Installing Dependencies"
 
 npm install
 
+# echo "Testing the Application"
+# artillery run test_endpoints.json
+
+# echo "Building the Docker Image"
 # docker build --tag bookapp:1.0 .
-# docker container run -d -p 8080:8080 bookapp:1.0
+# docker container run -d -p 3000:3000 bookapp:1.0
